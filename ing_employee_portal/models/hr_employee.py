c54d68736a966ece7f1abe1fe2bcd6ade0b945da
Raziel101 <81720812+Raziel101@users.noreply.github.com>
2025-08-12 10:02:57 -0300

    Add files via upload

--- ing_employee_portal/models/hr_employee.py
+++ ing_employee_portal/models/hr_employee.py
@@ -6,6 +6,12 @@ class HrEmployee(models.Model):
 
     department_id = fields.Many2one(string='Departamento', tracking=True)
     job_title = fields.Char(string='Título del trabajo', tracking=True)
+    license_class = fields.Many2many('ing.type.lic', string='Clases de licencias de conducir', tracking=True)
+    category_ids = fields.Many2many(
+        'hr.employee.category', 'employee_category_rel',
+        'emp_id', 'category_id',
+        groups="hr.group_hr_manager,ing_contratos.group_ing_rrhh_contratos_admin_rrhh,ing_contratos.group_ing_rrhh_contratos_encargado,ing_contratos.group_ing_rrhh_contratos_admin",
+        string='Tags', tracking=True)
 
     def get_job(self):
         return self.job_title if self.view_job_title else self.job_title_for
