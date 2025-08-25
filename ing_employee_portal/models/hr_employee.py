b66f251472e74c3b37091c4026814993dbd8853d
Raziel101 <81720812+Raziel101@users.noreply.github.com>
2025-08-25 10:26:55 -0300

    Add files via upload

--- ing_employee_portal/models/hr_employee.py
+++ ing_employee_portal/models/hr_employee.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     department_id = fields.Many2one(string='Departamento', tracking=True)
     job_title = fields.Char(string='Título del trabajo', tracking=True)
-    license_class = fields.Many2many('ing.type.lic', string='Clases de licencias de conducir', tracking=True)
+    license_class = fields.Many2many('ing.type.lic', string='Clases de licencias de conducir', tracking=True, store=True)
     category_ids = fields.Many2many(
         'hr.employee.category', 'employee_category_rel',
         'emp_id', 'category_id',
         groups="hr.group_hr_manager,ing_contratos.group_ing_rrhh_contratos_admin_rrhh,ing_contratos.group_ing_rrhh_contratos_encargado,ing_contratos.group_ing_rrhh_contratos_admin",
-        string='Tags', tracking=True)
+        string='Tags', tracking=True, store=True)
 
     def get_job(self):
         return self.job_title if self.view_job_title else self.job_title_for
@@ -26,3 +26,19 @@ class HrEmployee(models.Model):
         if user.tz:
             vals['tz'] = user.tz
         return vals
+
+    @api.model
+    def write(self, vals):
+        res = super(HrEmployee, self).write(vals)
+        for record in self:
+            if 'license_class' in vals:
+                new_vals = record.license_class.mapped('name')
+                record.message_post(
+                    body=f"Clases de licencia modificadas: {', '.join(new_vals)}"
+                )
+            if 'category_ids' in vals:
+                new_vals = record.category_ids.mapped('name')
+                record.message_post(
+                    body=f"Categorías modificadas: {', '.join(new_vals)}"
+                )
+        return res
